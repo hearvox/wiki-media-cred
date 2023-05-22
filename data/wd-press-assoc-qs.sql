320498d6b4da01fd220a6388a317fcded033ca03
Barrett Golding <mbox@hearingvoices.com>
2023-05-22 17:29:33 -0600

    Add other Press Associations

--- data/wd-press-assoc-qs.sql
+++ data/wd-press-assoc-qs.sql
@@ -26,3 +26,311 @@ LAST	P856	"https://ananews.com/"	P407	Q1860
 LAST	P213	"0000 0001 0489 6658"
 LAST	P244	"n77014568"
 LAST	P214	"157082104"
+CREATE
+LAST	Len	"Arkansas Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Arkansas"
+LAST	Aen	"arkansaspress.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1612
+LAST	P159	Q33405
+LAST	P856	"https://arkansaspress.org/"	P407	Q1860
+LAST	P214	"138608698"
+LAST	P213	"0000 0000 9420 8750"
+LAST	P244	"n2009163630"
+CREATE
+LAST	Len	"Florida Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Florida"
+LAST	Aen	"flpress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q812
+LAST	P159	Q37043
+LAST	P856	"https://flpress.com/"	P407	Q1860
+LAST	P244	"no90013939"
+LAST	P214	"0000 0001 3865 2161"
+CREATE
+LAST	Len	"Georgia Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Georgia"
+LAST	Aen	"gapress.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1428
+LAST	P159	Q2090004
+LAST	P856	"https://gapress.org/"	P407	Q1860
+LAST	P244	"no95034726"
+LAST	P214	"123453273"
+LAST	P213	"0000 0000 8545 2604"
+CREATE
+LAST	Len	"Newspaper Association of Idaho"
+LAST	Den	"Organization of newspapers in the U.S. state of Idaho"
+LAST	Aen	"newspaperassociationofidaho.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1221
+LAST	P159	Q35775
+LAST	P856	"https://www.newspaperassociationofidaho.com/"	P407	Q1860
+CREATE
+LAST	Len	"Hoosier State Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Indiana"
+LAST	Aen	"hspa.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1415
+LAST	P159	Q6346
+LAST	P856	"https://www.hspa.com/"	P407	Q1860
+LAST	P244	"n85261618"
+LAST	P214	"0000 0001 5367 6999"
+CREATE
+LAST	Len	"Kansas Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Kansas"
+LAST	Aen	"kspress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1558
+LAST	P159	Q41057
+LAST	P856	"https://kspress.com/"	P407	Q1860
+LAST	P244	"n86854575"
+LAST	P214	"139644758"
+LAST	P213	"0000 0000 9480 5774"
+CREATE
+LAST	Len	"Kentucky Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Kentucky"
+LAST	Aen	"kypress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1603
+LAST	P159	Q28603
+LAST	P856	"https://www.kypress.com/"	P407	Q1860
+LAST	P244	"n83051719"
+LAST	P214	"146615990"
+LAST	P213	"0000 0000 9883 8848"
+CREATE
+LAST	Len	"Louisiana Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Louisiana"
+LAST	Aen	"lapress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1588
+LAST	P159	Q28218
+LAST	P856	"https://www.lapress.com/"	P407	Q1860
+LAST	P244	"n85239467"
+LAST	P214	"148994661"
+LAST	P213	"0000 0001 0021 7515"
+CREATE
+LAST	Len	"Maine Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Maine"
+LAST	Aen	"mainepressassociation.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q724
+LAST	P159	Q2007905
+LAST	P856	"https://mainepressassociation.org/"	P407	Q1860
+LAST	P244	"n85018943"
+LAST	P214	"124393330"
+LAST	P213	"0000 0000 8597 7531"
+CREATE
+LAST	Len	"MDDC Press Association"
+LAST	Den	"Organization of newspapers in Washington, D.C. and the U.S. states of Maryland and Delaware"
+LAST	Aen	"mddcpress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1391
+LAST	P159	Q5092
+LAST	P856	"https://mddcpress.com/"	P407	Q1860
+CREATE
+LAST	Len	"Montana Newspaper Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Montana"
+LAST	Aen	"mtnewspapers.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1212
+LAST	P159	Q38733
+LAST	P856	"https://www.mtnewspapers.com/"	P407	Q1860
+CREATE
+LAST	Len	"Nebraska Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Nebraska"
+LAST	Aen	"nebpress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1553
+LAST	P159	Q28260
+LAST	P856	"https://nebpress.com/"	P407	Q1860
+LAST	P244	"n90672509"
+LAST	P214	"152575212"
+LAST	P213	"0000 0001 0229 8865"
+CREATE
+LAST	Len	"New Jersey Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of New Jersey"
+LAST	Aen	"njpa.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1408
+LAST	P159	Q7810345
+LAST	P856	"http://www.njpa.org/"	P407	Q1860
+CREATE
+LAST	Len	"New Mexico Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of New Mexico"
+LAST	Aen	"nmpress.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1522
+LAST	P159	Q34804
+LAST	P856	"https://www.nmpress.org/"	P407	Q1860
+CREATE
+LAST	Len	"North Dakota Newspaper Association"
+LAST	Den	"Organization of newspapers in the U.S. state of North Dakota"
+LAST	Aen	"ndna.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1207
+LAST	P159	Q37066
+LAST	P856	"https://www.ndna.com/"	P407	Q1860
+LAST	P244	"no97084469"
+LAST	P214	"134003497"
+LAST	P213	"0000 0000 9155 3954"
+CREATE
+LAST	Len	"Ohio News Media Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Ohio"
+LAST	Aen	"ohionews.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1397
+LAST	P159	Q16567
+LAST	P856	"https://www.ohionews.org/"	P407	Q1860
+LAST	P244	"n83022112"
+LAST	P214	"0000 0001 5363 9398"
+CREATE
+LAST	Len	"Oklahoma Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Oklahoma"
+LAST	Aen	"okpress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1649
+LAST	P159	Q34863
+LAST	P856	"https://okpress.com/"	P407	Q1860
+LAST	P244	"n2005173549"
+LAST	P214	"135081092"
+LAST	P213	"0000 0000 9216 503X"
+CREATE
+LAST	Len	"South Carolina Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of South Carolina"
+LAST	Aen	"scpress.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1456
+LAST	P159	Q38453
+LAST	P856	"https://www.scpress.org/"	P407	Q1860
+LAST	P244	"no2014086590"
+LAST	P214	"309843788"
+LAST	P213	"0000 0004 0502 7683"
+CREATE
+LAST	Len	"Texas Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Texas"
+LAST	Aen	"texaspress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1439
+LAST	P159	Q16559
+LAST	P856	"https://www.texaspress.com/"	P407	Q1860
+LAST	P244	"no94031879"
+LAST	P214	"144542570"
+LAST	P213	"0000 0000 9765 2128"
+CREATE
+LAST	Len	"Utah Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Utah"
+LAST	Aen	"utahpress.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q829
+LAST	P159	Q52466
+LAST	P856	"https://utahpress.com/"	P407	Q1860
+LAST	P244	"no2012141423"
+LAST	P214	"0000 0002 8208 9178"
+CREATE
+LAST	Len	"Vermont Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Vermont"
+LAST	Aen	"vtpress.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q16551
+LAST	P159	Q978718
+LAST	P856	"https://www.vtpress.org/"	P407	Q1860
+CREATE
+LAST	Len	"Virginia Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Virginia"
+LAST	Aen	"vpa.net"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1370
+LAST	P159	Q1374751
+LAST	P856	"https://www.vpa.net/"	P407	Q1860
+LAST	P244	"no95012408"
+LAST	P214	"0000 0001 5740 1493"
+CREATE
+LAST	Len	"West Virginia Press Association"
+LAST	Den	"Organization of newspapers in the U.S. state of West Virginia"
+LAST	Aen	"wvpress.org"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1371
+LAST	P159	Q44564
+LAST	P856	"https://wvpress.org/"	P407	Q1860
+CREATE
+LAST	Len	"Wisconsin Newspaper Association"
+LAST	Den	"Organization of newspapers in the U.S. state of Wisconsin"
+LAST	Aen	"wnanews.com"
+LAST	P31	Q22679796
+LAST	P31	Q2178147
+LAST	P452	Q11030
+LAST	P17	Q30
+LAST	P131	Q1537
+LAST	P159	Q43788
+LAST	P856	"https://wnanews.com/"	P407	Q1860
+LAST	P213	"0000 00004 0458 4795"
